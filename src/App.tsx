c0979bc82a8ddd047c4396f8a4c55a2f81e79d85
Žofie Tůmová <zofietumova@gmail.com>
2024-04-09 00:06:36 +0200

    fetch

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,28 @@ import { useEffect, useState } from "react";
 const App = () => {
   const [latitude, setLatitude] = useState([]);
   const [longitude, setLongitude] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
-    });
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      });
 
-    console.log(`Latitude is: ${latitude}`);
-    console.log(`Longitude is ${longitude}`);
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/
+    ?latitude=${latitude}&longitude=${longitude}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+        .then((response) => response.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
+    // console.log(`Latitude is: ${latitude}`);
+    // console.log(`Longitude is ${longitude}`);
+    fetchData();
   }, [latitude, longitude]);
+
   return <div className="App" />;
 };
 
