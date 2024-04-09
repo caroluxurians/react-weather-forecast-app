dcb0394e5e13d34ed086a42fb95c0863e46482f1
Žofie Tůmová <zofietumova@gmail.com>
2024-04-09 21:46:31 +0200

    lat,lon, weather object final

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import { useEffect, useState } from "react";
 // test: key: ef0494f73cfc0bd0feeb383fc9b32268
 
 const App = () => {
-  const [latitude, setLatitude] = useState([]);
-  const [longitude, setLongitude] = useState([]);
-  const [data, setData] = useState([]);
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [data, setData] = useState(0);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setLatitude(position.coords.latitude);
-        setLongitude(position.coords.longitude);
-      });
-
       await fetch(
         `${"https://api.openweathermap.org/data/2.5"}/weather/?lat=${latitude}&lon=${longitude}&units=metric&APPID=${"ef0494f73cfc0bd0feeb383fc9b32268"}`
       )
@@ -23,9 +25,12 @@ const App = () => {
           console.log(result);
         });
     };
-    console.log(`Latitude is: ${latitude}`);
-    console.log(`Longitude is ${longitude}`);
-    fetchData();
+
+    if (latitude && longitude) {
+      fetchData();
+      console.log(`Latitude is: ${latitude}`);
+      console.log(`Longitude is ${longitude}`);
+    }
   }, [latitude, longitude]);
 
   return <div className="App" />;
