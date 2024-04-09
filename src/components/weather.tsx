5cb0936002150a58ff9ddb8ec04f72e30ef3c256
Žofie Tůmová <zofietumova@gmail.com>
2024-04-09 23:22:37 +0200

    card contains more information

--- src/components/weather.tsx
+++ src/components/weather.tsx
@@ -6,7 +6,13 @@ const Weather = ({ weatherData }) => {
   return (
     <Card>
       <Card.Content>
-        <Card.Header className="header">{weatherData.name}</Card.Header>
+        <Card.Header className="header">
+          City name: {weatherData.name}
+        </Card.Header>
+        <p>Temprature: {weatherData.main.temp}</p>
+        <p>Sunrise: {weatherData.sys.sunrise}</p>
+        <p>Sunset: {weatherData.sys.sunset}</p>
+        <p>Description: {weatherData.weather[0].description}</p>
       </Card.Content>
     </Card>
   );
