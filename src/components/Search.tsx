d7d1a17932a71fc8c12b3d93d53939c3460542b7
Žofie Tůmová <zofietumova@gmail.com>
2024-04-15 18:27:03 +0200

    autocomplete

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,15 +1,40 @@
-import React from "react";
+import { useState } from "react";
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
+
+const loadOptions = (inputValue, setOptions) => {
+  return fetch(
+    `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=100000&namePrefix=${inputValue}`,
+    {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "f3e13718b2mshcc4ae55dc7fd047p11cd0ejsn6816aa8db411",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+      },
+    },
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      const formattedResult = result.data.map((city) => {
+        return {
+          id: `${city.latitude} ${city.longitude}`,
+          name: `${city.name}, ${city.countryCode}`,
+        };
+      });
+      console.log(formattedResult);
+      setOptions(formattedResult);
+    })
+    .catch((err) => console.error(err));
+};
 
 const Search = () => {
+  const [options, setOptions] = useState([]);
+  console.log(options)
   return (
     <div>
-      <form className="form flex">
-        <input
-          className="searchbar"
-          placeholder=" Type your city"
-          type="text"
-        />
-      </form>
+      <ReactSearchAutocomplete
+        onSearch={(val) => loadOptions(val, setOptions)}
+        items={options}
+      />
     </div>
   );
 };
